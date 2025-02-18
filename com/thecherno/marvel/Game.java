b623cb3bda067442c7a9bcfae70d9a3d4e5ff68a
Marvel Azer <1038600@mvusd.net>
2025-02-18 12:19:50 -0800

    add anew things

--- com/thecherno/marvel/Game.java
+++ com/thecherno/marvel/Game.java
@@ -1,20 +1,35 @@
 package com.thecherno.marvel;
 
-public class Game implements Runnable {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
 	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	
 	private Thread thread;
+	private JFrame frame;
 	private boolean running = false;
 	
+	public Game() {
+		Dimension size = new Dimension(width*scale, height*scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
+	
 	public synchronized void start() {
+		running = true;
 		thread = new Thread(this, "Display");
 		thread.start();
 	}
 	
 	public synchronized void stop() {
+		running = false;
 		try {
 			thread.join();
 		} catch(InterruptedException e ) {
@@ -24,6 +39,17 @@ public class Game implements Runnable {
 	}
 	
 	public void run() {
+		while (running) {
+		}
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("Rain");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
 
