cc0949502f78f68b44d615f32b3cb60406bf2cc2
Marvel Azer <1038600@mvusd.net>
2025-02-21 12:19:32 -0800

    add a black color to the window

--- com/thecherno/marvel/Game.java
+++ com/thecherno/marvel/Game.java
@@ -1,7 +1,9 @@
 package com.thecherno.marvel;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
@@ -59,7 +61,11 @@ public class Game extends Canvas implements Runnable {
 			return;
 		}
 		
-		
+		Graphics g = bs.getDrawGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect( 0, 0, getWidth(), getHeight());
+		g.dispose();
+		bs.show();
 	}
 	
 	public static void main(String[] args) {
