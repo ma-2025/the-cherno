74980162dead382dd91911dff703d61c976a775c
Marvel Azer <1038600@mvusd.net>
2025-02-27 12:00:01 -0800

    making a new folder called screen

--- com/thecherno/marvel/Game.java
+++ com/thecherno/marvel/Game.java
@@ -10,6 +10,8 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import com.thecherno.marvel.graphics.Screen;
+
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 	
@@ -20,6 +22,8 @@ public class Game extends Canvas implements Runnable {
 	private Thread thread;
 	private JFrame frame;
 	private boolean running = false;
+	
+	private Screen screen;
 
 	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
@@ -27,6 +31,7 @@ public class Game extends Canvas implements Runnable {
 	public Game() {
 		Dimension size = new Dimension(width*scale, height*scale);
 		setPreferredSize(size);
+		screen = new Screen(width, height);
 		
 		frame = new JFrame();
 	}
