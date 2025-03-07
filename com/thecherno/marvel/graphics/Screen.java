e19a349192edeeb18096a59f5dc077e9863a3bfa
Marvel Azer <1038600@mvusd.net>
2025-03-06 17:08:36 -0800

    make the animation move correctly

--- com/thecherno/marvel/graphics/Screen.java
+++ com/thecherno/marvel/graphics/Screen.java
@@ -5,13 +5,13 @@ public class Screen {
 	private int width, height;
 	public int[] pixels;
 	
-	int time = 0;
+	int xtime = 0, ytime = 50;
 	int counter = 0;
 	
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
-		pixels = new int[width * height];
+		pixels = new int[width * height]; //0 - 50,399 = 50,400
 	}
 	
 	public void clear ()
@@ -23,15 +23,14 @@ public class Screen {
 	
 	public void render() {
 	counter++;
-	if(counter % 100 ==0) {
-		time++;
-	}
+	if(counter % 10 == 0) xtime++ ;
+	if(counter % 80 == 0) ytime++ ;
+	
 		for(int y = 0; y < height; y++) {
+			if(ytime >= height) break;
 			for(int x = 0; x < width; x++) {
-				pixels[x + y * width] = 0xff00ff; 
-				throw new ArrayIndexOutOfBoundsException();
-				
-				
+				if(xtime >= width) break;
+				pixels[xtime + ytime * width] = 0xff00ff; 
 			}
 		}
 	}
