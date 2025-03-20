89c892838bedf2192099539aecc5f082ae4c4c42
Marvel Azer <1038600@mvusd.net>
2025-03-20 12:17:57 -0700

    make a new file and name it SPRITSHEET

---
+++ com/thecherno/marvel/graphics/SpriteSheet.java
@@ -0,0 +1,15 @@
+package com.thecherno.marvel.graphics;
+
+public class SpriteSheet {
+	
+	private String path;
+	private final int SIZE;
+	public int[] pixels;
+	
+	public SpriteSheet (String path, int size)
+	{
+		this.path = path;
+		this.SIZE = size;
+	}
+
+}
