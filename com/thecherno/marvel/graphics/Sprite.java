b7000d41c04f8b96c606c3cb849bfc36802f8d5e
Marvel Azer <1038600@mvusd.net>
2025-04-08 12:06:18 -0700

    make a private void called sheet

--- com/thecherno/marvel/graphics/Sprite.java
+++ com/thecherno/marvel/graphics/Sprite.java
@@ -9,10 +9,19 @@ public class Sprite {
 	
 	public Sprite(int size, int x, int y, SpriteSheet sheet) {
 		SIZE = size;
+		pixels = new int [SIZE * SIZE];
 		this.x = x * size;
 		this.y = y * size;
 		this.sheet = sheet;
 		load();
+	}
+	
+	private void load() {
+		for ( int y = 0; y < SIZE; y++) {
+			for ( int x = 0; x < SIZE; x++) {
+				pixels[x + y * SIZE] = sheet.pixels[ (x + this.x ) + (y + this.y) * sheet.SIZE ];
+			}
+		}
 	}
 	
 }
